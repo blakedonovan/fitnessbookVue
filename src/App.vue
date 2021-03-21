d0c73ee678c168ef65d4ec7dbf8f99d2b27bcf75
Tobias <atlan084@gmail.com>
2021-03-21 21:34:24 +0100

    VueMaterialSetup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <FitnessBook msg="Fitnessbook Admin Panel"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FitnessBook from './components/FitnessBook.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FitnessBook
   }
 }
 </script>
