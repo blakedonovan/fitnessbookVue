46cd182708c55b7fb60cb6b0678b7881f3638157
blakedonovan <atlan084@gmail.com>
2021-04-04 19:23:22 -0400

    addedInput

--- src/components/cookbook/ingredients.vue
+++ src/components/cookbook/ingredients.vue
@@ -2,13 +2,48 @@
 <template>
  <div>
    
-<div class="col-2">
-    <b-form-input v-model="search" placeholder="Zutat suchen" class="mb-1 pl-1" >
+<div class="col-auto">
+  
+    <b-form-input v-model="search" placeholder="Zutat suchen" class="mb-2 pl-1 " >
 
     </b-form-input>
   
   </div>
+<div>
+  <b-button v-b-toggle.collapse-1 variant="success" size="sm">Zutat hinzufügen</b-button>
+  <b-collapse id="collapse-1" class="mt-2 mb-3">
+  <div>
+  <b-form inline >
+    <label class="sr-only" for="inline-form-input-Zutat">Zutat</label>
+    <b-form-input
+      id="inline-form-input-Zutat"
+      class="mb-2 mr-sm-2 mb-sm-0 sm"
+      placeholder="Zutat"
+    ></b-form-input>
+
+    <label class="sr-only" for="inline-form-input-Fett">Fett</label>
+    <b-input-group class="mb-2 mr-sm-2 mb-sm-0">
+      <b-form-input id="inline-form-input-Fett" placeholder="Fett"></b-form-input>
+    </b-input-group>
+
+   <label class="sr-only" for="inline-form-input-Kohlenhydrate">Kohlenhydrate</label>
+    <b-input-group class="mb-2 mr-sm-2 mb-sm-0">
+      <b-form-input id="inline-form-input-Kohlenhydrate" placeholder="Kohlenhydrate"></b-form-input>
+    </b-input-group>
+
+    <label class="sr-only" for="inline-form-input-Protein">Protein</label>
+    <b-input-group class="mb-2 mr-sm-2 mb-sm-0">
+      <b-form-input id="inline-form-input-Protein" placeholder="Protein"></b-form-input>
+    </b-input-group>
+
+    <b-button variant="primary" class="xs">Speichern</b-button>
+  </b-form>
+</div>
+  </b-collapse>
+</div>
+  
 <b-table  
+ 
 
  sticky-header
 :items="ingridients" 
@@ -16,9 +51,9 @@
 :sort-by.sync="sortBy"
 :sort-desc.sync="sortDesc"
  sort-icon-right
-
+ dark="true"
 :filter=search
-lazy
+ lazy
 >
 
 </b-table>
@@ -40,8 +75,9 @@ import axios from 'axios'
   errors:'',
  sortBy: 'Zutat',
  sortDesc: false,
+ 
 
-    ingridients:'',
+    ingridients:[],
     nutritionCat: [
       {key:'',sortable:false},
       { key: 'Zutat', sortable: true },
@@ -66,7 +102,7 @@ computed:{
 
       const response = await axios.get('http://localhost:8000/cookbook/ingredients')
       this.ingridients = response.data
-      //console.log(response.data) // uncomment if data loads
+      //console.log(response.data) // uncomment if data loads 
 
       }catch(e){
     
@@ -74,5 +110,12 @@ computed:{
 
       }
       }
+
+
+      
   }
+
+  
 </script>
+
+
--- src/components/cookbook/recipes.vue
+++ src/components/cookbook/recipes.vue
@@ -48,6 +48,7 @@
   export default {
     data() {
       return {
+        locale:0,
         value: 4.2
       }
     }
