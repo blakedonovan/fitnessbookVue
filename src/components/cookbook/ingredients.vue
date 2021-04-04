72de866c93ae743928d6c9a9f35600bf46f6dc87
blakedonovan <atlan084@gmail.com>
2021-04-04 13:02:29 -0400

    changes

--- src/components/cookbook/ingredients.vue
+++ src/components/cookbook/ingredients.vue
@@ -1,3 +1,78 @@
+
 <template>
-    <h2>Zutaten</h2>
+ <div>
+   
+<div class="col-2">
+    <b-form-input v-model="search" placeholder="Zutat suchen" class="mb-1 pl-1" >
+
+    </b-form-input>
+  
+  </div>
+<b-table  
+
+ sticky-header
+:items="ingridients" 
+:fields="nutritionCat"
+:sort-by.sync="sortBy"
+:sort-desc.sync="sortDesc"
+ sort-icon-right
+
+:filter=search
+lazy
+>
+
+</b-table>
+
+  
+   </div>
+   
+  
+  
 </template>
+
+<script>
+import axios from 'axios'
+  export default {
+    data() {
+      
+      return {
+  search:'',   // search string for table 
+  errors:'',
+ sortBy: 'Zutat',
+ sortDesc: false,
+
+    ingridients:'',
+    nutritionCat: [
+      {key:'',sortable:false},
+      { key: 'Zutat', sortable: true },
+      { key: 'Fett', sortable: true },
+      { key: 'Kohlenhydrate', sortable: true },
+      { key: 'Protein', sortable: true },
+      { key: 'Kcal', sortable: true }
+        ],
+
+      }
+    },
+
+computed:{
+
+},
+
+
+    async mounted(){
+      
+      try{
+  
+
+      const response = await axios.get('http://localhost:8000/cookbook/ingredients')
+      this.ingridients = response.data
+      //console.log(response.data) // uncomment if data loads
+
+      }catch(e){
+    
+    this.errors.push(e)
+
+      }
+      }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import { BootstrapVue, IconsPlugin ,BootstrapVueIcons} from 'bootstrap-vue'
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
@@ -11,6 +12,9 @@ import router from './router'
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false
 Vue.use(BootstrapVueIcons);
+
+Vue.use(VueAxios, axios)
+
 new Vue({
 BootstrapVue,
 IconsPlugin,
